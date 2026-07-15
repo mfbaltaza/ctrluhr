600aa66344e40b3140b242b424effab569cf8535
Juan Araujo <araujogreydiego@gmail.com>
2026-07-14 21:48:16 -0400

    scaffold: tanstack router

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,10 +1,19 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { queryClient } from "#/lib/query-client";
 
 import appCss from "../styles.css?url";
 
-export const Route = createRootRoute({
+import type { QueryClient } from "@tanstack/react-query";
+
+
+export interface RouterContext {
+	queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
 	head: () => ({
 		meta: [
 			{
@@ -35,7 +44,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 				<HeadContent />
 			</head>
 			<body>
-				{children}
+				<QueryClientProvider client={queryClient}>
+					{children}
+				</QueryClientProvider>
 				<TanStackDevtools
 					config={{
 						position: "bottom-right",
